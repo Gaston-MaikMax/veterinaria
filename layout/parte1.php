09caa4c3c185ed39d437cc86b4691855a428f34b
Mike <gastoncondori97@gmail.com>
2024-08-29 18:41:05 -0400

    feat: Actualizar enlaces de reserva de citas en la página de inicio

--- layout/parte1.php
+++ layout/parte1.php
@@ -1,14 +1,34 @@
+<?php
+session_start();
+if (isset($_SESSION['sesion_email'])) {
+  // echo "ha pasdo por logeado";
+  $email_sesion = $_SESSION['sesion_email'];
+  $sql = "SELECT * FROM tb_usuarios WHERE email = '$email_sesion'";
+  $query = $pdo->prepare($sql);
+  $query->execute();
+  $usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+  foreach ($usuarios as $usuario) {
+    $id_usuario_sesion = $usuario['id_usuario'];
+    $cargo_sesion = $usuario['cargo'];
+  }
+} else {
+  // echo "no a pasado por el login";
+  header('location:' . $URL . '/login');
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="public/css/bootstrap.min.css">
-  <link rel="stylesheet" href="public/css/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="public/css/style.css">
-  <link rel="stylesheet" href="/public//css/style.css">
+  <link rel="stylesheet" href="<?php echo $URL; ?>/public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $URL; ?>/public/css/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="<?php echo $URL; ?>/https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="<?php echo $URL; ?>/public/css/style.css">
+  <link rel="stylesheet" href="<?php echo $URL; ?>//public//css/style.css">
   <script src="public/js/jquery-3.7.1.slim.min.js"></script>
 
   <title>Sistema de Veterinario</title>
@@ -18,8 +38,8 @@
 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container">
-      <a class="navbar-brand " href="index.php" style="display: flex; align-items: center; gap:5px;">
-        <img src="public/images/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
+      <a class="navbar-brand " href="<?php echo $URL; ?>" style="display: flex; align-items: center; gap:5px;">
+        <img src="../public/images/logo.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top">
         Sabueso
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -60,8 +80,8 @@
                 Registar
               </a>
               <ul class="dropdown-menu ">
-                <li><a class="dropdown-item" href="login/index.php">Inisiar Sesion</a></li>
-                <li><a class="dropdown-item" href="login">Registarate</a></li>
+                <li><a class="dropdown-item" href="<?php echo $URL; ?>/login/index.php">Inisiar Sesion</a></li>
+                <li><a class="dropdown-item" href="<?php echo $URL; ?>/login/registro.php">Registarate</a></li>
 
               </ul>
             </li>
