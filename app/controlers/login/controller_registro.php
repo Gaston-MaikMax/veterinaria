1d72c8b031674b126c79ffd9f207d295dc51e352
Mike <gastoncondori97@gmail.com>
2024-09-05 19:09:15 -0400

    feat: Agregar archivo cargar_reservas.php

---
+++ app/controlers/login/controller_registro.php
@@ -0,0 +1,60 @@
+<?php
+include('../../../app/config.php');
+
+$nombre_completo = $_POST['nombre_completo'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password_repetido = $_POST['password_repetido'];
+
+
+
+$cargo = "CLIENTE";
+
+$contador = 0;
+$sql = "SELECT * FROM tb_usuarios WHERE email = '$email'";
+$query = $pdo->prepare($sql);
+$query->execute();
+$usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($usuarios as $usuario) {
+  $contador = $contador + 1;
+}
+if ($contador > 0) {
+
+  session_start();
+  $_SESSION['mensaje'] = "El coreo electonico" . $email . " ya esta registrado";
+  $_SESSION['icono'] = "error";
+  header('Location: ' . $URL . '/login/registro.php');
+} else {
+
+  if ($password == $password_repetido) {
+
+    $password = password_hash($password, PASSWORD_DEFAULT);
+
+    // echo "son iguakles";
+    $sentencia = $pdo->prepare("INSERT INTO tb_usuarios (nombre_completo, email, password, cargo, fyh_creacion) VALUES (:nombre_completo, :email, :password, :cargo , :fyh_creacion)");
+    $sentencia->bindParam('nombre_completo', $nombre_completo);
+    $sentencia->bindParam('email', $email);
+    $sentencia->bindParam('password', $password);
+    $sentencia->bindParam('cargo', $cargo);
+    $sentencia->bindParam('fyh_creacion', $fechaHora);
+
+    if ($sentencia->execute()) {
+      session_start();
+      $_SESSION['mensaje'] = "se registro el usuaria" . $nombre_completo . " corectamente";
+      $_SESSION['icono'] = "success";
+      header('Location: ' . $URL . '/');
+    } else {
+      session_start();
+      $_SESSION['mensaje'] = "erro al registrar el usuario";
+      $_SESSION['icono'] = "error";
+      header('Location: ' . $URL . '/login/registro.php');
+    }
+  } else {
+    // contyraceñas no son iguales
+    session_start();
+    $_SESSION['mensaje'] = "las contraseñas no son iguales";
+    $_SESSION['icono'] = "error";
+    header('Location: ' . $URL . '/login/registro.php');
+  }
+}
